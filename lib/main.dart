13ed054ebc1250e847f4c31bb76d3d50b1121957
Muhammad Zohaib <muhammad.zohaib@evampsaanga.com>
2022-04-05 01:16:24 +0500

    adminweb configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:let_go_gb/modules/admin/admin_application.dart';
@@ -14,14 +16,26 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
+const firebaseConfig = {
+  "apiKey": "AIzaSyBAfrEGDzwbA5ei1gnKDvH3XNWIqEJvITY",
+  "authDomain": "letsgogb-6eb5b.firebaseapp.com",
+  "projectId": "letsgogb-6eb5b",
+  "storageBucket": "letsgogb-6eb5b.appspot.com",
+  "messagingSenderId": "756450874654",
+  "appId": "1:756450874654:web:6775bbd6517870920ee50f"
+};
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      /* options: DefaultFirebaseOptions.currentPlatform,*/
-      );
+    options: FirebaseOptions.fromMap(firebaseConfig),
+  );
   await GetStorage.init();
   HttpOverrides.global = MyHttpOverrides();
   await UserDefaults.getPref();
+
+  if (kIsWeb) {
+    await FirebaseCoreWeb().initializeApp();
+  }
 
   //runApp(const DriverApplication());
   // runApp(const UserApplication());
